b2b81233cee40787cb1db23e7cff4e01c8cc23a7
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-08-10 14:36:51 +0300

    get_fft_spec() created

--- audio/example02.py
+++ audio/example02.py
@@ -12,20 +12,24 @@ import plotly.graph_objs as go
 layout = go.Layout(title='Spectrogram Calculation Example',
                    xaxis=dict(title='time (sec)',),
                    yaxis=dict(title='Freqs (Hz)',))
-if __name__ == '__main__':
-    [fs, s] = wavfile.read("../data/sample_music.wav")
-    win = 0.02
-    frame_size, signal_len, spec, time_axis = int(win * fs), len(s), [], []
+
+
+def get_fft_spec(signal, fs, win):
+    frame_size, signal_len, spec, times = int(win * fs), len(signal), [], []
     # break signal into non-overlapping short-term windows (frames)
-    frames = np.array([s[x:x + frame_size] for x in
+    frames = np.array([signal[x:x + frame_size] for x in
                        np.arange(0, signal_len - frame_size, frame_size)])
     for i_f, f in enumerate(frames): # for each frame
-        time_axis.append(i_f * win)
+        times.append(i_f * win)
         # append mag of fft
         X = np.abs(scp.fft(f)) ** 2
         freqs = np.arange(0, 1, 1.0/len(X)) * (fs/2)
         spec.append(X[0:int(len(X)/2)] / X.max())
-    spec = np.array(spec).T
-    heatmap = go.Heatmap(z=spec, y=freqs, x=time_axis)
+    return np.array(spec).T, freqs, times
+
+if __name__ == '__main__':
+    [Fs, s] = wavfile.read("../data/sample_music.wav")
+    S, f, t = get_fft_spec(s, Fs, 0.02)
+    heatmap = go.Heatmap(z=S, y=f, x=t)
     plotly.offline.plot(go.Figure(data=[heatmap], layout=layout),
                         filename="temp.html", auto_open=True)
