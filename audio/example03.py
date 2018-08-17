a19d18bdabb576a52314748b5efe5dafd654df89
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-08-17 22:11:14 +0300

    recorded file is written to wav file at ctr+c

--- audio/example03.py
+++ audio/example03.py
@@ -3,20 +3,28 @@
 @details Example of audio recording and spectrogram computation
 @author Theodoros Giannakopoulos {tyiannak@gmail.com}
 """
-import example02
 import numpy as np
-import pyaudio, struct, cv2
+import scipy.io.wavfile as wavfile
+import example02, pyaudio, struct, cv2, signal, sys
+
 fs = 8000
 bufSize = int(fs * 1.0)
+def signal_handler(signal, x):
+    wavfile.write("output.wav", fs, np.int16(aggregated_buf))
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
 if __name__ == '__main__':
-    # initialize recording:
-    pa = pyaudio.PyAudio()
+    global aggregated_buf; aggregated_buf = np.array([])
+    pa = pyaudio.PyAudio() # initialize recording
     stream = pa.open(format=pyaudio.paInt16 , channels=1, rate=fs,
                      input=True, frames_per_buffer=bufSize)
     while 1:
         # read recorded data, convert bytes to samples and then to numpy array
         block = stream.read(bufSize, exception_on_overflow=False)
-        s = np.array(list(struct.unpack("%dh"%(len(block)/2), block))) / (2**15)
+        s = np.array(list(struct.unpack("%dh"%(len(block)/2), block)))
+        aggregated_buf = np.concatenate((aggregated_buf, s))
+        s /= (2 ** 15)
         # get spectrogram and visualize it using opencv
         specgram, f, t = example02.get_fft_spec(s, fs, 0.02)
         iSpec = np.array(specgram[::-1] * 255, dtype=np.uint8)
