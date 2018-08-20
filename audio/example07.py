9fb7935e7fd5c02a65481ac52f513ff47f743fef
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-08-20 12:17:12 +0300

    minor

--- audio/example07.py
+++ audio/example07.py
@@ -4,20 +4,15 @@
 @author Theodoros Giannakopoulos {tyiannak@gmail.com}
 """
 from __future__ import print_function
-import numpy as np
-import scipy.io.wavfile as wavfile
-import os, time
+import os, time, scipy.io.wavfile as wavfile, numpy as np
 from random import randint
-import playsound
 
 def play_sound(freq, duration, fs):
-    t = np.arange(0, duration, 1.0/fs)  # time range
-    x = 0.5*np.cos(2 * np.pi * t * freq)    # signal
-    wavfile.write("temp.wav", fs, x)
-    playsound.playsound("temp.wav")
+    t = np.arange(0, duration, 1.0/fs);  x = 0.5*np.cos(2 * np.pi * t * freq)
+    wavfile.write("temp.wav", fs, x); os.system("play temp.wav -q")
 
 if __name__ == '__main__':
-    freqs, thres, n_exp = [250, 500, 1000, 2000, 3000], [2, 5, 10, 20], 10
+    freqs, thres, n_exp, fs = [250, 500, 1000, 2000, 3000], [2, 5, 10, 20], 10, 16000
     answers = [[] for i in range(len(freqs))]
     for i_f, f in enumerate(freqs):
         for t in thres:
@@ -25,16 +20,11 @@ if __name__ == '__main__':
             for i in range(n_exp):
                 sequel = randint(1, 2)
                 if sequel == 2:
-                    play_sound(f,   0.5, 16000)
-                    time.sleep(0.5)
-                    play_sound(f+t, 0.5, 16000)
+                    play_sound(f, 0.5, fs); time.sleep(0.5); play_sound(f+t, 0.5, fs)
                 else:
-                    play_sound(f+t, 0.5, 16000)
-                    time.sleep(0.5)
-                    play_sound(f,   0.5, 16000)
+                    play_sound(f+t, 0.5, fs); time.sleep(0.5); play_sound(f, 0.5, fs)
                 ans = int(raw_input('Which was higher (1/2):'))
-                if ans == sequel:
-                    answers[i_f][-1] += 1
+                if ans == sequel: answers[i_f][-1] += 1
     print("Freq\t", end='')
     for t in thres:
         print("{0:.1f}\t".format(t), end='')
@@ -44,4 +34,3 @@ if __name__ == '__main__':
         for i_t, t in enumerate(thres):
             print("{0:.1f}\t".format(answers[i_f][i_t] / float(n_exp)), end='')
         print("")
-
