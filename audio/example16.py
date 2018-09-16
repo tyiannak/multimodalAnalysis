ba9374a3594e99a336651cce65d54141e2ea5186
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-09-17 01:27:08 +0300

    code refactoring

--- audio/example16.py
+++ audio/example16.py
@@ -4,24 +4,23 @@
 songs from different musical genres
 @author Theodoros Giannakopoulos {tyiannak@gmail.com}
 """
-import numpy as np
-import scipy.io.wavfile as wavfile
-import glob, os, librosa, plotly
-from pyAudioAnalysis import audioFeatureExtraction as aF
-import plotly.graph_objs as go
-import utilities as ut
+import scipy.io.wavfile as wavfile, utilities as ut
+import glob, os, librosa, plotly, numpy as np, plotly.graph_objs as go
+from pyAudioAnalysis.audioFeatureExtraction import mtFeatureExtraction as mt
 
+layout = go.Layout(title='Beat and spectral centroid distributions',
+                   xaxis=dict(title='Tempo (bpms)',),
+                   yaxis=dict(title='Spectral Centroid Mean',))
 
 def get_dir_features(dir_name):
     feats = []
     for f in glob.glob(os.path.join(dir_name, "*.wav")):
         [Fs, s] = wavfile.read(f)
         tempo, _ = librosa.beat.beat_track(y=s, sr=Fs)
-        f, _, fn = aF.mtFeatureExtraction(s, Fs, int(1.0 * Fs), int(1.0 * Fs),
-                                          int(0.1 * Fs), int(0.1 * Fs))
-        feats.append([tempo, np.mean(f[fn.index("spectral_centroid_mean")], axis=0)])
+        f, _, fn = mt(s, Fs, int(1.0*Fs), int(1.0*Fs), int(0.1*Fs), int(0.1*Fs))
+        feats.append([tempo, np.mean(f[fn.index("spectral_centroid_mean")],
+                                     axis=0)])
     return np.array(feats)
-
 
 if __name__ == '__main__':
     g_paths = glob.glob("../data/musical_genres_small/*/")
@@ -32,4 +31,5 @@ if __name__ == '__main__':
                         mode='markers', name=g_names[i], marker=dict(
                         color=clr[i], size=15))
              for i in range(len(g_paths))]
-    plotly.offline.plot(plots, filename="temp.html", auto_open=True)
+    plotly.offline.plot(go.Figure(data = plots, layout=layout),
+                        filename="temp.html", auto_open=True)
