b4d66139766eba4cdc05ab97e6394083a39d6e8c
tyiannak <tyiannak@gmail.com>
2018-09-27 17:41:49 +0300

    pitch computation change

--- audio/example17.py
+++ audio/example17.py
@@ -8,22 +8,24 @@ import librosa
 import plotly
 import numpy as np
 import plotly.graph_objs as go
+from scipy.signal import medfilt as mf
 
 def get_librosa_pitch(signal, fs, window):
-    pitches, magnitudes = librosa.piptrack(y=signal, sr=fs, n_fft=int(window))
+    pitches, magnitudes = librosa.piptrack(y=signal, sr=fs, n_fft=int(window),
+                                           hop_length=int(window/10))
     pitch_pos = np.argmax(magnitudes, axis=0)
     pitches_final = []
     for i in range(len(pitch_pos)):
         pitches_final.append(pitches[pitch_pos[i], i])
     pitches_final = np.array(pitches_final)
-    pitches_final[pitches_final > 800] = 0
-    return pitches_final
+    pitches_final[pitches_final > 500] = 0
+    return mf(pitches_final, 3)
 
 if __name__ == '__main__':
     [fs, s1] = wavfile.read("../data/speech_male_sample.wav")
     [fs, s2] = wavfile.read("../data/speech_female_sample.wav")
-    p1 = get_librosa_pitch(s1, fs, fs/10)
-    p2 = get_librosa_pitch(s2, fs, fs/10)
+    p1 = get_librosa_pitch(s1, fs, fs/20)
+    p2 = get_librosa_pitch(s2, fs, fs/20)
     plt1 = go.Scatter(x=np.arange(len(p1)), y=p1, mode='markers',
                       showlegend=False)
     plt2 = go.Scatter(x=np.arange(len(p2)), y=p2, mode='markers',
