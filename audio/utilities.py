af3877849c6bc1e618cae5e31c2f201892d25366
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-10-07 22:22:14 +0300

    overfitting example (generic) added. 21

--- audio/utilities.py
+++ audio/utilities.py
@@ -7,6 +7,9 @@ import plotly
 import plotly.graph_objs as go
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
 
 def plot_feature_histograms(list_of_feature_mtr, feature_names,
                             class_names, n_columns=5):
@@ -55,3 +58,76 @@ def get_color_combinations(n_classes):
                                               clr_map(range_cl[i])[2],
                                               clr_map(range_cl[i])[3]))
     return clr
+
+
+def compute_class_rec_pre_f1(c_mat):
+    '''
+    :param c_mat: the [n_class x n_class] confusion matrix
+    :return: rec, pre and f1 for each class
+    '''
+    n_class = c_mat.shape[0]
+    rec, pre, f1 = [], [], []
+    for i in range(n_class):
+        rec.append(float(c_mat[i, i]) / np.sum(c_mat[i, :]))
+        pre.append(float(c_mat[i, i]) / np.sum(c_mat[:, i]))
+        f1.append(2 * rec[-1] * pre[-1] / (rec[-1] + pre[-1]))
+    return rec,  pre, f1
+
+
+def svm_train_evaluate(X, y, k_folds, C=1):
+    '''
+    Trains and evaluate an SVM classifier
+    :param X: Feature matrix
+    :param y: Labels matrix
+    :param k_folds: Number of folds
+    :param C: SVM C param
+    :return: confusion matrix, average f1 measure and overall accuracy
+    '''
+    # normalize
+    mean, std = X.mean(axis=0), np.std(X, axis=0)
+    X = (X - mean) / std
+    # k-fold evaluation:
+    kf = KFold(n_splits=k_folds, shuffle=True)
+    count_cm = 0
+    f1s = []
+    accs = []
+    for train, test in kf.split(X):
+        x_train, x_test, y_train, y_test = X[train], X[test], y[train], y[test]
+        cl = SVC(kernel='rbf', C=C)
+        cl.fit(x_train, y_train)
+        y_pred = cl.predict(x_test)
+        # update aggregated confusion matrix:
+        if count_cm == 0:
+            cm = confusion_matrix(y_pred=y_pred, y_true=y_test)
+        else:
+            cm += (confusion_matrix(y_pred=y_pred, y_true=y_test))
+        count_cm += 1
+        f1s.append(f1_score(y_pred=y_pred, y_true=y_test, average='micro'))
+        accs.append(accuracy_score(y_pred=y_pred, y_true=y_test))
+    f1 = np.mean(f1s)
+    acc = np.mean(accs)
+    return cm, f1, acc
+
+
+def plotly_classification_results(cm, class_names):
+    heatmap = go.Heatmap(z=np.flip(cm, axis=0), x=class_names,
+                         y=list(reversed(class_names)),
+                         colorscale=[[0, '#4422ff'], [1, '#ff4422']],
+                         name="confusin matrix", showscale=False)
+    rec, pre, f1 = compute_class_rec_pre_f1(cm)
+    mark_prop1 = dict(color='rgba(150, 180, 80, 0.5)',
+                      line=dict(color='rgba(150, 180, 80, 1)', width=2))
+    mark_prop2 = dict(color='rgba(140, 200, 120, 0.5)',
+                      line=dict(color='rgba(140, 200, 120, 1)', width=2))
+    mark_prop3 = dict(color='rgba(50, 150, 220, 0.5)',
+                      line=dict(color='rgba(50, 150, 220, 1)', width=3))
+    b1 = go.Bar(x=class_names,  y=rec, name="rec", marker=mark_prop1)
+    b2 = go.Bar(x=class_names,  y=pre, name="pre", marker=mark_prop2)
+    b3 = go.Bar(x=class_names,  y=f1, name="f1", marker=mark_prop3)
+    figs = plotly.tools.make_subplots(rows=1, cols=2,
+                                      subplot_titles=["Confusion matrix",
+                                                      "Performance measures"])
+    figs.append_trace(heatmap, 1, 1); figs.append_trace(b1, 1, 2)
+    figs.append_trace(b2, 1, 2); figs.append_trace(b3, 1, 2)
+    plotly.offline.plot(figs, filename="temp.html", auto_open=True)
+
