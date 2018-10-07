0ff24499142c7cb2c81e2690bb239e8f47b45747
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-10-07 22:59:55 +0300

    musical genre classification example (20) update

--- audio/example20.py
+++ audio/example20.py
@@ -3,31 +3,23 @@
 @details Musical genre classification example. Classification performance
 @author Theodoros Giannakopoulos {tyiannak@gmail.com}
 """
-import numpy as np, plotly.graph_objs as go
+import numpy as np
+import utilities as ut
 from pyAudioAnalysis.audioFeatureExtraction import dirWavFeatureExtraction as dW
-from sklearn.svm import SVC
-from sklearn.model_selection import KFold
-from sklearn.metrics import confusion_matrix, f1_score, accuracy_score
+
 
 if __name__ == '__main__':
-    f1, _, fn1 = dW("../data/musical_genres_8k/blues", 2, 1, 0.1, 0.1)
-    f2, _, fn2 = dW("../data/musical_genres_8k/electronic", 2, 1, 0.1, 0.1)
-    f3, _, fn3 = dW("../data/musical_genres_8k/jazz", 2, 1, 0.1, 0.1)
+    # extract features, concatenate feature matrices and normalize:
+    mw, stw = 2, .1
+    f1, _, fn1 = dW("../data/musical_genres_8k/blues", mw, mw, stw, stw)
+    f2, _, fn2 = dW("../data/musical_genres_8k/electronic", mw, mw, stw, stw)
+    f3, _, fn3 = dW("../data/musical_genres_8k/jazz", mw, mw, stw, stw)
     x = np.concatenate((f1, f2, f3), axis=0)
-    mean, std = x.mean(axis=0), np.std(x, axis=0)
-    x = (x - mean) / std
-
     y = np.concatenate((np.zeros(f1.shape[0]), np.ones(f2.shape[0]),
                        2 * np.ones(f2.shape[0])))
-    kf = KFold(n_splits=10, shuffle=True)
-    count_cm = 0
-    for train, test in kf.split(x):
-        x_train, x_test, y_train, y_test = x[train], x[test], y[train], y[test]
-        cl = SVC(kernel='rbf', C=1)
-        cl.fit(x_train, y_train)
-        y_pred = cl.predict(x_test)
-        if count_cm == 0: cm = confusion_matrix(y_pred=y_pred, y_true=y_test)
-        else: cm += (confusion_matrix(y_pred=y_pred, y_true=y_test))
-        count_cm += 1
-    print(cm)
+    # train svm and get aggregated (average) confusion matrix, accuracy and f1
+    cm, acc, f1 = ut.svm_train_evaluate(x, y, 10, C=2)
+    # visualize performance measures
+    ut.plotly_classification_results(cm, ["blues", "electronic", "jazz"])
+    print(acc, f1)
 
