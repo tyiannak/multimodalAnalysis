02e1f69f9bb507f437d6539a065bb93f0a5825ce
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-10-20 23:11:24 +0300

    minor

--- visual/example01.py
+++ visual/example01.py
@@ -1,6 +1,7 @@
 """!
 @brief Example 1
-@details: Load a jpeg image and show it, using opencv
+@details: Load a jpeg image and show it, using opencv, along with the average
+values of the RGB coefficients
 @author Theodoros Giannakopoulos {tyiannak@gmail.com}
 """
 import cv2
@@ -9,15 +10,15 @@ import matplotlib.pyplot as plt
 def img_read_and_plot_rgb_values(filename):
     img = cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB)
     print(img.shape)
-    print(
-    "R range: {} - {} mu={}".format(img[:, :, 0].min(), img[:, :, 0].max(),
-                                    img[:, :, 0].mean()))
-    print(
-    "G range: {} - {} mu={}".format(img[:, :, 1].min(), img[:, :, 1].max(),
-                                    img[:, :, 1].mean()))
-    print(
-    "B range: {} - {} mu={}".format(img[:, :, 2].min(), img[:, :, 2].max(),
-                                    img[:, :, 2].mean()))
+    print("R range: {} - {} mu={}".format(img[:, :, 0].min(),
+                                          img[:, :, 0].max(),
+                                          img[:, :, 0].mean()))
+    print("G range: {} - {} mu={}".format(img[:, :, 1].min(),
+                                          img[:, :, 1].max(),
+                                          img[:, :, 1].mean()))
+    print("B range: {} - {} mu={}".format(img[:, :, 2].min(),
+                                          img[:, :, 2].max(),
+                                          img[:, :, 2].mean()))
     plt.imshow(img)
     plt.show()
 
