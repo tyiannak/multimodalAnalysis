3fe6183ed3df3fdf8266e8455ea66f5bf5aa2174
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-10-27 01:27:01 +0300

    minor

--- visual/example08.py
+++ visual/example08.py
@@ -1,6 +1,8 @@
 """!
 @brief Example 8
 @details: FFT2 Examples  - visual interpretation of FFT2
+(log is not used for visualization reasons - the examples need to look quite
+"binary")
 @author Theodoros Giannakopoulos {tyiannak@gmail.com}
 """
 import cv2
@@ -10,7 +12,7 @@ from scipy import fftpack
 
 def demo_fft_2(file_path):
     img = cv2.cvtColor(cv2.imread(file_path), cv2.COLOR_BGR2GRAY)
-    fft = np.log(np.abs(fftpack.fftshift(fftpack.fft2(img))))
+    fft = (np.abs(fftpack.fftshift(fftpack.fft2(img))))
     h, w = fft.shape
     c1, c2 = int(h / 2), int(w / 2)
     fft_cut = fft[c1 - int(0.025 * h): c1 + int(0.025 * h),
@@ -21,7 +23,7 @@ im3, fft3, fft_z2 = demo_fft_2('../data/images_general/sin3.png')
 im4, fft4, fft_z4 = demo_fft_2('../data/images_general/sin4.png')
 im5, fft5, fft_z5 = demo_fft_2('../data/images_general/sin5.png')
 plt.subplot(4, 2, 1); plt.imshow(im1, cmap='gray'); plt.title("image")
-plt.subplot(4, 2, 2); plt.imshow(fft_z1, cmap='jet'); plt.title("fft x20 zoom")
+plt.subplot(4, 2, 2); plt.imshow(fft_z1, cmap='gray'); plt.title("fft x20 zoom")
 plt.subplot(4, 2, 3); plt.imshow(im3, cmap='gray'); plt.title("image")
 plt.subplot(4, 2, 4); plt.imshow(fft_z2, cmap='gray'); plt.title("fft x20 zoom")
 plt.subplot(4, 2, 5); plt.imshow(im4, cmap='gray'); plt.title("image")
