d4a03d8bd7184e978e88a0ccc266803415474a3d
Theodoros Giannakopoulos <tyiannak@gmail.com>
2018-12-10 23:52:52 +0200

    fingerprint examples added (search)

---
+++ audio/fingerprinting_search_song.py
@@ -0,0 +1,23 @@
+"""! 
+@brief fingerprinting search song
+@details: Audio fingerprinting using dejavu
+@author Theodoros Giannakopoulos {tyiannak@gmail.com}
+"""
+
+import sys
+sys.path.append('../')
+import dejavu
+from dejavu.recognize import FileRecognizer
+
+config = {
+    "database": {
+        "host": "127.0.0.1",
+        "user": "root",
+        "passwd": "",
+        "db": "dejavu",
+    }
+}
+
+if __name__ == '__main__':
+    djv = dejavu.Dejavu(config)
+    print djv.recognize(FileRecognizer, "query.wav")
