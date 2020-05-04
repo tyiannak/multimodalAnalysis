f3ad1590f0f17806adc27dccf443941f38e42395
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-05-04 23:25:03 +0300

    update to pyAudioAnalysis

--- audio/example28.py
+++ audio/example28.py
@@ -6,8 +6,8 @@
 import os, readchar, sklearn.cluster
 from pyAudioAnalysis.MidTermFeatures import mid_feature_extraction as mT
 from pyAudioAnalysis.audioBasicIO import read_audio_file, stereo_to_mono
-from pyAudioAnalysis.audioSegmentation import flags2segs
-from pyAudioAnalysis.audioTrainTest import normalizeFeatures
+from pyAudioAnalysis.audioSegmentation import labels_to_segments
+from pyAudioAnalysis.audioTrainTest import normalize_features
 
 if __name__ == '__main__':
     # read signal and get normalized segment features:
@@ -16,18 +16,18 @@ if __name__ == '__main__':
     x = stereo_to_mono(x)
     mt_size, mt_step, st_win = 1, 0.1, 0.05
     [mt_feats, st_feats, _] = mT(x, fs, mt_size * fs, mt_step * fs,
-                                round(fs * st_win), round(fs * st_win * 0.5))
-    (mt_feats_norm, MEAN, STD) = normalizeFeatures([mt_feats.T])
+                                 round(fs * st_win), round(fs * st_win * 0.5))
+    (mt_feats_norm, MEAN, STD) = normalize_features([mt_feats.T])
     mt_feats_norm = mt_feats_norm[0].T
     # perform clustering (k = 4)
     n_clusters = 4
     k_means = sklearn.cluster.KMeans(n_clusters=n_clusters)
     k_means.fit(mt_feats_norm.T)
     cls = k_means.labels_
-    segs, c = flags2segs(cls, mt_step)      # convert flags to segment limits
+    segs, c = labels_to_segments(cls, mt_step) # convert flags to segment limits
     for sp in range(n_clusters):            # play each cluster's segment
         for i in range(len(c)):
-            if c[i] == sp and segs[i, 1]-segs[i, 0] > 1:
+            if c[i] == sp and segs[i, 1]-segs[i, 0] > 0.5:
                 # play long segments of current speaker
                 print(c[i], segs[i, 0], segs[i, 1])
                 cmd = "avconv -i {} -ss {} -t {} temp.wav " \
