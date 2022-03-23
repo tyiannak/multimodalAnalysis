c96063ed9da9a7df47d0453cda22851d88abafc2
Theodoros Giannakopoulos <tyiannak@gmail.com>
2022-03-24 00:23:58 +0200

    feature normalization using sklearn scaler

--- audio/example28.py
+++ audio/example28.py
@@ -7,7 +7,6 @@ import os, readchar, sklearn.cluster
 from pyAudioAnalysis.MidTermFeatures import mid_feature_extraction as mT
 from pyAudioAnalysis.audioBasicIO import read_audio_file, stereo_to_mono
 from pyAudioAnalysis.audioSegmentation import labels_to_segments
-from pyAudioAnalysis.audioTrainTest import normalize_features
 
 if __name__ == '__main__':
     # read signal and get normalized segment features:
@@ -18,8 +17,11 @@ if __name__ == '__main__':
     [mt_feats, st_feats, _] = mT(x, fs, mt_size * fs, mt_step * fs,
                                  round(fs * st_win), round(fs * st_win * 0.5))
     print(mt_feats.shape)
-    (mt_feats_norm, MEAN, STD) = normalize_features([mt_feats.T])
-    mt_feats_norm = mt_feats_norm[0].T
+#    (mt_feats_norm, MEAN, STD) = normalize_features([mt_feats.T])
+#    mt_feats_norm = mt_feats_norm[0].T
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    mt_feats_norm = scaler.fit_transform(mt_feats.T).T
     # perform clustering (k = 4)
     n_clusters = 4
     k_means = sklearn.cluster.KMeans(n_clusters=n_clusters)
